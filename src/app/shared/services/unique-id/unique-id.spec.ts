3f0864578980c7e247ea312b4ec73945d370cadd
= <=>
2022-04-24 12:33:46 -0300

    Finalizado o capítulo 02. Lapidando nossos testes

--- src/app/shared/services/unique-id/unique-id.spec.ts
+++ src/app/shared/services/unique-id/unique-id.spec.ts
@@ -45,4 +45,13 @@ describe(UniqueIdService.name, () => {
     expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
   });
 
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, '', '0', '1'];
+    emptyValues.forEach(emptyValue => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+        .withContext(`Empty value ${emptyValue}`)
+        .toThrow();
+    });
+  });
+
 });
